b0b68fe010ac9d4e891c8acbca7b87dff9c1f1f5
Abdul0247 <133867229+Abdul0247@users.noreply.github.com>
2023-07-20 15:43:17 +0100

    Create Python Programming Day 15.py

--- Python Programming Day 1.py
+++ Python Programming Day 1.py
@@ -1,5 +1,20 @@
-print("Hello World")
+print("hello world")
 
-print("hello" + "world")
+print('DAy1-python print Function')
+print("The function is decleared like this")
+print("print("what to print")")
 
-print('hello "+" world')
+print("Hello"+ " "+ "Angela")
+print("hello" + input("what is your name?"))
+
+
+name = "jack"
+print(name)
+
+name = "Angela"
+print(name)
+
+
+name= input("what is your name?")
+lenght=len(name)
+print(lenght)
